7547ca5699b8a561a410488047172307c4e14c76
Srikalyan Megati <kalmegati@gmail.com>
2019-11-26 20:04:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/").last
-      item = @@items.detect{|i| i.name == item}
+      item = @@items.detect{|i| i.name == item_name}
     else
       resp.write "Route not found"
       resp.status = 404
