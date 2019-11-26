31840277d0dd6f3e12103075b89e7779ed7419f7
Srikalyan Megati <kalmegati@gmail.com>
2019-11-26 19:56:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path.split("/").first == "items"
+    if req.path.match(/items/)
       
     else
       resp.write "Route not found"
